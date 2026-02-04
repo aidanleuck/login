6f7126a715af0f78b3cc3408f5811d7644c8efd3
aidanleuck <aidan@leucks.com>
2026-02-03 18:38:19 -0700

    fix: try re-route

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   publicDir: "images",
-  base: "./"
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "404.html")
+      }
+    }
+  }
 });
